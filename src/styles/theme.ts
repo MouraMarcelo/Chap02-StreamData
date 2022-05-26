6a6675041713876b1c65229fba62a8cef8b9c0d6
MouraMarcelo <marcelo.mc999@gmail.com>
2022-05-26 18:26:15 -0300

    Finished!

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    purple: '#9146FF',
+    purple_darker: '#833FE5',
+
+    black: '#0E0E10',
+
+    white: '#FFF',
+    
+    gray: '#CFCFCF'
+  },
+
+  fonts: {
+    regular: 'DMSans_400Regular',
+    bold: 'DMSans_700Bold'
+  }
+}
+
+export default theme;
